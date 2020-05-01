6b8fc8603827a4602318d718f68450fc6cff7a1c
Glitch (hello-express) <none>
2020-05-01 09:49:10 +0000

    🍮🔦 Checkpoint ./views/transaction/create.pug:15639022/824 ./Routes/transaction.route.js:15639022/1465 ./db.js:15639022/1 ./db.json:15639022/21

--- Routes/transaction.route.js
+++ Routes/transaction.route.js
@@ -5,10 +5,21 @@ const db = require("../db.js");
 
 //get transaction
 router.get("/", (req, res) => {
-  const transactions = db.get("transaction").value();
-  
+  const transactions = db.get("transactions").value();
+
   res.render("transaction/index.pug", {
     transactions
+  });
+});
+
+//get form transaction
+router.get("/create", (req, res) => {
+  const users = db.get("users").value();
+  const books = db.get("books").value();
+
+  res.render("transaction/create.pug", {
+    users,
+    books
   });
 });
 
