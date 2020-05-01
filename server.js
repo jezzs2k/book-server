a82307518e19ea3357926a9ba4b0f2ba7d75dc06
Glitch (hello-express) <none>
2020-05-01 02:18:59 +0000

    🙎🚜 Checkpoint ./server.js:15639022/3024 ./package.json:15639022/2771

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
+const shortid = require("shortid");
 
-app.set('view engine', 'pug')
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-// https://expressjs.com/en/starter/basic-routing.html
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ books: [] }).write();
+
+app.set("view engine", "pug");
+
 app.get("/", (request, response) => {
-  response.render('./index.pug');
+  response.render("./index.pug");
 });
 
-app.get('/todos', (req, res) =>{
-  res.render('./todos/index.pug')
-})
- 
+//get books
+app.get("/books", (req, res) => {
+  const books = db.get("books").__wrapped__.books;
+  res
+});
 
-// listen for requests :)
+//create new book {id: , description: , title: }
+app.post("/books", (req, res) => {});
+
+//deletebookby id
+app.delete("/books/:id/delete", (req, res) => {});
+
+//updatebookby id
+app.put("/books/:id/update", (req, res) => {});
+
 app.listen(process.env.PORT, () => {
   console.log("Server listening on port " + process.env.PORT);
 });
