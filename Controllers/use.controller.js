31306f79bca83a49c10f76b2c4f7f61fd787ae53
Glitch (hello-express) <none>
2020-05-09 06:04:14 +0000

    🏐🌄 Checkpoint ./Controllers/use.controller.js:15639022/11882 ./views/users/profile.pug:15639022/39 ./server.js:15639022/6

--- Controllers/use.controller.js
+++ Controllers/use.controller.js
@@ -1,15 +1,14 @@
 const shortid = require("shortid");
-const cloudinary = require('cloudinary');
+const cloudinary = require("cloudinary");
 
 //config
-cloudinary.config({ 
-  cloud_name: 'dt9wztdih', 
-  api_key: '256121794527994', 
-  api_secret: 'hxDjOIKc77lkOqeHWB8wlSmnAHk' 
+cloudinary.config({
+  cloud_name: "dt9wztdih",
+  api_key: "256121794527994",
+  api_secret: "hxDjOIKc77lkOqeHWB8wlSmnAHk"
 });
 
 const db = require("../db.js");
-
 
 module.exports.getUser = (req, res) => {
   const users = db.get("users").value();
@@ -23,7 +22,7 @@ module.exports.profile = (req, res) => {
   res.render("users/profile.pug", {
     users
   });
-}
+};
 
 module.exports.deleteUser = (req, res) => {
   const id = req.params.id;
@@ -45,25 +44,22 @@ module.exports.getUpdateUser = (req, res) => {
   res.render("users/update", { user });
 };
 
-module.exports.postUpdateUser = (req, res) => {
+module.exports.postUpdateUser = async (req, res) => {
   const id = req.params.id;
-  
-  const urlImg = req.file.path.split('/').splice(1).join('/');
 
- 
-  
-  
-  cloudinary.uploader.upload(`https://enchanted-phase-cushion.glitch.me/${urlImg}`, function(error, result) {
-    console.log(result);
-    return;
-    db.get("users")
+  const urlImg = req.file.path
+    .split("/")
+    .splice(1)
+    .join("/");
+
+  const result = await cloudinary.uploader.upload(
+    `https://enchanted-phase-cushion.glitch.me/${urlImg}`
+  );
+
+  db.get("users")
     .find({ id })
-    .assign({ ...req.body, avatar: result.path })
+    .assign({ ...req.body, avatar: result.secure_url })
     .write();
-  });
-
-  
-  
 
   res.redirect("/users");
 };
